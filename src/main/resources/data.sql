79683fc358070257ebe949239d650cbd11101225
Ahmed Al Salih <ahmed82@users.noreply.github.com>
2020-11-30 01:32:49 -0500

    compleate configure the project port, database, loggin, JPA, and dummy data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into employee (address, first_name, last_name, phone) values  ('NH', 'Ahmed', 'Al-Salih','6032899431');
+insert into employee (address, first_name, last_name, phone) values  ('NC', 'Zianb', 'ALhashemy','1234567');
+insert into employee (address, first_name, last_name, phone) values  ('NC', 'Ali', 'Alla-Aleek','1234567');
+insert into employee (address, first_name, last_name, phone) values  ('NC', 'Aaron', 'Alison','1234567');
+insert into employee (address, first_name, last_name, phone) values  ('NC', 'Sean' , 'Thomson', '1234567');
+insert into employee (address, first_name, last_name, phone) values  ('NC', 'Brian ', 'Brean','1234567');
+insert into employee (address, first_name, last_name, phone) values  ('NY', 'Tim ', 'Smith','1234567'); 
